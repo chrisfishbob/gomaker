cb85fb50ce331b47400102427899acd0e8bae3da
Chris <chrisfishbob@gmail.com>
2022-01-11 11:41:36 -0800

    Added timestamp feature to show total compilation time

--- gomaker.go
+++ gomaker.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
-	// Import go routines
 	"sync"
+	"time"
 )
 
 // Checks if the file is a valid c or cpp file
@@ -15,7 +15,8 @@ func isValidFile(f os.FileInfo) bool {
 	return !f.IsDir() && strings.Contains(f.Name(), ".c") && strings.Count(f.Name(), ".") == 1
 }
 
-func compileFiles() {
+func processFiles() {
+	start := time.Now()
 	files_compiled := 0
 
 	files, err := ioutil.ReadDir(".")
@@ -31,34 +32,41 @@ func compileFiles() {
 		go func(file os.FileInfo) {
 			// We defer wg.Done() to decrement waitgroup regarless of validity of name
 			defer wg.Done()
-			
+
 			if isValidFile(file) {
-				var cmd *exec.Cmd
-				var output_name string
-	
-				if strings.Contains(file.Name(), "cpp") {
-					output_name = strings.TrimSuffix(file.Name(), ".cpp")
-					cmd = exec.Command("g++", file.Name(), "-o", output_name)
-				} else {
-					output_name = strings.TrimSuffix(file.Name(), ".c")
-					cmd = exec.Command("gcc", file.Name(), "-o", output_name)
-				}
-	
-				fmt.Println("Executing:", cmd)
-				cmd.Stderr = os.Stderr
-				cmd.Run()
-	
-				exec.Command("mv", output_name, "output/").Run()
-	
-				files_compiled++
+				runCompileCommand(file, &files_compiled)
 			}
 		}(file)
 	}
 
-	wg.Wait()
-
-	println("Compiled", files_compiled, "files")
+	// Gathering all the goroutines
+	wg.Wait()  
+	end := time.Now()
+	fmt.Println("Compiled", files_compiled, "files in", end.Sub(start).Seconds(), "seconds")
 }
+
+func runCompileCommand(file os.FileInfo, files_compiled *int) {
+	var cmd *exec.Cmd
+	var output_name string
+
+	if strings.Contains(file.Name(), "cpp") {
+		output_name = strings.TrimSuffix(file.Name(), ".cpp")
+		cmd = exec.Command("g++", file.Name(), "-o", output_name)
+	} else {
+		output_name = strings.TrimSuffix(file.Name(), ".c")
+		cmd = exec.Command("gcc", file.Name(), "-o", output_name)
+	}
+
+	fmt.Println("Executing:", cmd)
+	// Ensures that warnings and errors are printed
+	cmd.Stderr = os.Stderr
+	cmd.Run()
+
+	exec.Command("mv", output_name, "output/").Run()
+
+	(*files_compiled)++
+}
+
 
 func createOutputFolder() {
 	exec.Command("mkdir", "output").Run()
@@ -66,7 +74,6 @@ func createOutputFolder() {
 
 func main() {
 	createOutputFolder()
-	compileFiles()
+	processFiles()
 	fmt.Print("Compilation complete.")
-
 }
