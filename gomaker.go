77c2b6098d60198d4d9530d26db9411504b318ef
Chris <chrisfishbob@gmail.com>
2022-01-13 21:09:21 -0800

    Added function to unzip zipfiles, not yet implemented into the main loop

--- gomaker.go
+++ gomaker.go
@@ -1,15 +1,76 @@
 package main
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"sync"
 	"time"
 )
+
+func Unzip(src string, dest string) ([]string, error) {
+
+    var filenames []string
+
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return filenames, err
+    }
+    defer r.Close()
+
+    for _, f := range r.File {
+
+        // Store filename/path for returning and using later on
+        fpath := filepath.Join(dest, f.Name)
+
+        // Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+        if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+            return filenames, fmt.Errorf("%s: illegal file path", fpath)
+        }
+
+        filenames = append(filenames, fpath)
+
+        if f.FileInfo().IsDir() {
+            // Make Folder
+            os.MkdirAll(fpath, os.ModePerm)
+            continue
+        }
+
+        // Make File
+        if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+            return filenames, err
+        }
+
+        outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+        if err != nil {
+            return filenames, err
+        }
+
+        rc, err := f.Open()
+        if err != nil {
+            return filenames, err
+        }
+
+        _, err = io.Copy(outFile, rc)
+
+        // Close the file without defer to close before next iteration of loop
+        outFile.Close()
+        rc.Close()
+
+        if err != nil {
+            return filenames, err
+        }
+    }
+    return filenames, nil
+}
+
+
 
 // Checks if the file is a valid c or cpp file
 func isValidFile(f os.FileInfo) bool {
@@ -67,6 +128,7 @@ func processFiles() {
 	fmt.Println(files_skipped_string)
 	fmt.Println("Compiled", files_compiled, "files in", end.Sub(start).Seconds(), "seconds")
 }
+
 
 func runCompileCommand(file os.FileInfo, files_compiled *int, string_slice *[]string, stderr_slice *[]string) {
 	var cmd *exec.Cmd
